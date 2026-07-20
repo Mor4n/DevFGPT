74660688304f3ec34bd18d126357dae9abd96d75
Mor4n <anm0ran@outlook.com>
2026-07-20 08:38:40 -0700

    Archivos iniciales de parte 2

---
+++ src/components/MessageBubble.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// ponerle color 2b2b2b si es de usuario, si es de chatbot, que se ponga abajo como chatgpt
+function MessageBubble() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MessageBubble
